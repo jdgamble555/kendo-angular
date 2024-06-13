3e60b57cf4b21ab7b031f9ede2022ed5f6d0ed3e
Jonathan Gamble <jdgamble555@gmail.com>
2024-06-13 18:24:07 -0500

    kendo

--- src/app/kendo-autocomplete/kendo-autocomplete.component.ts
+++ src/app/kendo-autocomplete/kendo-autocomplete.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 @Component({
   selector: 'app-kendo-autocomplete',
   standalone: true,
-  imports: [],
+  imports: [
+    LabelModule,
+    FormsModule,
+    InputsModule,
+    ReactiveFormsModule,
+    DropDownsModule,
+    ButtonsModule
+  ],
   templateUrl: './kendo-autocomplete.component.html',
   styleUrl: './kendo-autocomplete.component.css'
 })
 export class KendoAutocompleteComponent {
-
+  public areaList: Array<string> = [
+    "Amsterdam",
+    "Athens",
+    "Barcelona",
+    "Berlin",
+    "Brussels",
+    "Chicago",
+    "Copenhagen",
+    "Dublin",
+    "Helsinki",
+    "Houston",
+    "Lisbon",
+    "London",
+    "Los Angeles",
+    "Madrid",
+    "Miami",
+    "Montreal",
+    "New York",
+    "Paris",
+    "Philadelphia",
+    "Prague",
+    "Rome",
+    "Sao Paulo",
+    "Seattle",
+    "Stockholm",
+    "Toronto",
+    "Vancouver",
+    "Vienna",
+    "Vienna",
+    "Warsaw",
+  ];
 }
